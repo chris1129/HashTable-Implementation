13b0f98e65c39cfa30653a93db8bd6f89c1fa346
chris1129 <chris34346@gmail.com>
2018-04-23 18:33:36 -0400

    first commit

---
+++ implementHashTable/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  implementHashTable
+//
+//  Created by Sheng-Yung Cheng on 4/23/18.
+//  Copyright © 2018 Sheng-Yung Cheng. All rights reserved.
+//
+
+#include <iostream>
+#include "hashTable.h"
+using namespace std;
+int main(int argc, const char * argv[]) {
+    hashTable hash;
+    hash.put(0, 5);
+    hash.put(128,3);
+    hash.remove(128);
+    cout<<hash.get(128)<<endl;
+    cout<<hash.get(0)<<endl;
+    cout<<hash.get(4)<<endl;
+    return 0;
+}
